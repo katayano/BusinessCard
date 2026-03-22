3197c0559c8fc11bee3e54cd424a054f573b2347
katayano <idnlnroocwetn22@gmail.com>
2026-03-23 00:59:01 +0900

    連絡先を表示

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -5,16 +5,19 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Call
 import androidx.compose.material.icons.rounded.Email
+import androidx.compose.material.icons.rounded.Share
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
@@ -30,7 +33,9 @@ class MainActivity : ComponentActivity() {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     BusinessCard(
                         name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .fillMaxSize()
                     )
                 }
             }
@@ -40,16 +45,33 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun BusinessCard(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Column(
+            modifier = Modifier
+        ) {
+            ContactInfo(
+                icon = Icons.Rounded.Call,
+                info = "012-345-6789"
+            )
+            ContactInfo(
+                icon = Icons.Rounded.Share,
+                info = "@AndroidDev"
+            )
+            ContactInfo(
+                icon = Icons.Rounded.Email,
+                info = "example@android.com"
+            )
+        }
+    }
+
 }
 
 @Composable
 fun ContactInfo(icon: ImageVector, info: String) {
     Row(
-        horizontalArrangement = Arrangement.Center,
         modifier = Modifier.padding(8.dp)
     ) {
         Icon(
